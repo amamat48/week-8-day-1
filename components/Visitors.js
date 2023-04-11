7ffd7c621336dfb8607a1a426714827e6179b06f
Amari <amarimathis73@gmail.com>
2023-04-11 16:01:50 -0400

    finished

--- components/Visitors.js
+++ components/Visitors.js
@@ -1,8 +1,9 @@
 export default function Visitors() {
     return (
         <div className="visitors">
-            <h1>821</h1>
-            <image href="example.com"></image>
+            <h1>Visitors</h1>
+            <h2>821</h2>
+            <image href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2e6A9y8ml1PZ3_Ub14-6VDcfOs6nQkMxtjw&usqp=CAU"></image>
         </div>
     )
 }
